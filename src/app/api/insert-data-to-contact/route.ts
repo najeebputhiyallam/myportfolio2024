f7332af365fd172f52ba425f96381ec4f83be614
najeebputhiyallam <najeebputhiyallam@gmail.com>
2024-01-06 23:43:44 +0400

    changed favicon

--- src/app/api/insert-data-to-contact/route.ts
+++ src/app/api/insert-data-to-contact/route.ts
@@ -2,13 +2,14 @@ import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
  
 export async function GET(request: Request) {
+
   const { searchParams } = new URL(request.url);
   const name = searchParams.get('name');
   const email = searchParams.get('email');
   const message = searchParams.get('message');
- 
+
   try {
-    if (!name || !email) throw new Error('Pet and owner names required');
+    if (!name || !email || !message) throw new Error('Name, Email and Message required');
     await sql`INSERT INTO contact (Name, Email, Message) VALUES (${name}, ${email}, ${message});`;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
