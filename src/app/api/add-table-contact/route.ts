0be319843378c9936cab34ddb2275b57ff38b97e
najeebputhiyallam <najeebputhiyallam@gmail.com>
2024-01-06 22:47:19 +0400

    added table exit

--- src/app/api/add-table-contact/route.ts
+++ src/app/api/add-table-contact/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   try {
     const result =
-      await sql`CREATE TABLE contact ( Name varchar(255), Email varchar(255), Message varchar(255));`;
+      await sql`CREATE TABLE IF NOT EXIST contact ( Name varchar(255), Email varchar(255), Message varchar(255));`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
