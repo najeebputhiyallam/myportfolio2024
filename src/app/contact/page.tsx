781e756a2b8de1e0a70c8714b1077709b25d867a
najeebputhiyallam <najeebputhiyallam@gmail.com>
2024-01-07 22:56:44 +0400

    added server action form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,38 +1,52 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
 import { Metadata } from "next";
 import styles from '../contact/contact.module.css'
+
 
 export const metadata: Metadata = {
     title: "Contact",
 }
 
 export default function ContactPage(){
-    return(
-        <div className="containerGlobal" > 
-            <div className={` ${styles.containerWrapper} containerWrapperGlobal`} >
-                <h1 className="gradientText" >/ Contact</h1>
 
-                <form className={styles.form} >
+    const submitForm = async (formData) => {
+        "use server";
+        //console.log(formData.get('name'));
+        const nameValue = formData.get('name');
+        const emailValue = formData.get('email');
+        const messageValue = formData.get('message');
+        await sql`INSERT INTO contact (Name, Email, Message) VALUES (${nameValue}, ${emailValue}, ${messageValue});`;
+    }
+
+    return(
+
+        <form className={styles.form} action={submitForm} >
+            <div className="containerGlobal" > 
+                <div className={` ${styles.containerWrapper} containerWrapperGlobal`} >
+                    <h1 className="gradientText" >/ Contact</h1>
+
                     <div className={styles.formDiv}>
                         <label htmlFor="name" >Name</label>
-                        <input name="name" id="name" value="" placeholder="Your name" />
+                        <input type="text" name="name" placeholder="Your name" />
                     </div>
 
                     <div className={styles.formDiv}>
                         <label htmlFor="email" >Email</label>
-                        <input name="email" id="email" value="" placeholder="youremail@mail.com" />
+                        <input type="text" name="email" placeholder="youremail@mail.com" />
                     </div>
 
                     <div className={styles.formDiv}>
                         <label htmlFor="message" >Message</label>
-                        <textarea name="message" id="message" placeholder="Write your message" ></textarea>
+                        <textarea name="message" placeholder="Write your message" ></textarea>
                     </div>      
 
                     <div className={styles.formDiv}>
-                        <button >Submit</button>
-                    </div>                              
-                </form>
+                        <button type="submit" >Submit</button>
+                    </div>  
 
+                </div>
             </div>
-        </div>
+        </form>    
     );
 }
