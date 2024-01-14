43486de2c319ca90096303d402fbcff38468c4f2
najeebputhiyallam <najeebputhiyallam@gmail.com>
2024-01-15 01:44:35 +0400

    repushhh

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,9 +8,25 @@ export const metadata: Metadata = {
     title: "Projects",
 }
 
+async function getData(){
+    const res = await fetch(`${process.env.SITEBASEURL}api/project-list`, { method: 'GET' });
+    //const res = await fetch(`${process.env.SITEBASEURL}api/projects`)
+    //const res = await fetch("https://next.najeebdigital.xyz/api/projects")
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
 
-export default function ProjectsPage(){
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch data - Something wrong this page')
+    }
+   
+    return res.json()
+  }
 
+export default async function ProjectsPage(){
+
+    const data = await getData();
+    //console.log( data );
 
     return(
         <div className="containerGlobal" > 
@@ -19,7 +35,19 @@ export default function ProjectsPage(){
 
                 <div className={styles.projectContent} >
                     <div className={styles.projectBoxHolder} >
-
+                        
+                            { 
+                                data.map( (project: { id: string; img: string; url: string; title: string; }) => (
+                                    <div key={project.id} className={styles.projectBox} >
+                                        <div className={styles.projectBoxWrapper} >
+                                            <Image src={project.img} alt="" fill style={{objectFit: "contain", borderRadius: "0" }} />
+                                            <p>{project.title}</p>
+                                            <p><Link href={project.url} target='_blank'>{project.url}</Link></p>
+                                        </div>    
+                                    </div>
+                                ))                                
+                            }
+                        
                     </div>
 
                     <Button btnType="button" btnText="Contact me" btnLink="/contact" />
