1c70f378dbb1e812766bd9dd4d358c8c34116716
najeebputhiyallam <najeebputhiyallam@gmail.com>
2024-04-24 11:09:22 +0400

    updated dependencies

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,46 +1,55 @@
 import { Metadata } from "next";
-import styles from './skills.module.css'
+import styles from "./skills.module.css";
 import Button from "@/components/button/Button";
 
 export const metadata: Metadata = {
-    title: "Skills",
-}
+  title: "Skills",
+};
 
-export default function SkillsPage(){
-    return(
-        <div className="containerGlobal" > 
-            <div className={`${styles.containerWrapperGlobal} containerWrapperGlobal`} >
-                <h1 className="gradientText" >/ Skills</h1>
+export default function SkillsPage() {
+  return (
+    <div className="containerGlobal">
+      <div
+        className={`${styles.containerWrapperGlobal} containerWrapperGlobal`}
+      >
+        <h1 className="gradientText">/ Skills</h1>
 
-                <div className={styles.skillsContent} >
-                    <ul className={styles.skillsList} >
-                        <li>Next.js</li>
-                        <li>Elementor</li>
-                        <li>Wordpress</li>
-                        <li>WooCommerce</li>
-                        <li>Shopify</li>
-                        
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>Photoshop</li>
-                        <li>jQuery</li>
-                        <li>JavaScript</li>
-                        <li>SEO</li>
-                        
-                        <li>PHP</li>
-                        <li>MySQL</li>
-                        <li>Website Optimization</li>
-                        
-                        
-                        <li>Web Design</li>
-                        <li>Domain Management</li>
-                        <li>Figma</li>
-                    </ul>
-                    <p>In short, I can manage the entire A to Z process of website development, including selecting the domain, choosing the best hosting provider, and ultimately launching your website.</p>
+        <div className={styles.skillsContent}>
+          <ul className={styles.skillsList}>
+            <li>Next.js</li>
+            <li>Elementor</li>
+            <li>Wordpress</li>
+            <li>WooCommerce</li>
+            <li>Shopify</li>
 
-                    <Button btnType="button" btnText="View My Experience" btnLink="/experience" />
-                </div>                
-            </div>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Photoshop</li>
+            <li>jQuery</li>
+            <li>JavaScript</li>
+            <li>SEO</li>
+
+            <li>PHP</li>
+            <li>MySQL</li>
+            <li>Website Optimization</li>
+
+            <li>Web Design</li>
+            <li>Domain Management</li>
+            <li>Figma</li>
+          </ul>
+          <p>
+            In short, I can manage the entire A to Z process of website
+            development, including selecting the domain, choosing the best
+            hosting provider, and ultimately launching your website.
+          </p>
+
+          <Button
+            btnType="button"
+            btnText="View My Experience"
+            btnLink="/experience"
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
